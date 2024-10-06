3af9798daefe9d5cd1674c163c2ffe6a586809d9
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-10-06 15:40:46 +0600

    Fetch Pets by Category Done.

--- js/index.js
+++ js/index.js
@@ -22,6 +22,23 @@ function displayData(pets) {
     const dataContainer = document.getElementById('data-container');
     dataContainer.innerHTML = '';
 
+    if(pets == 0) {
+        dataContainer.classList.remove("grid");
+        dataContainer.innerHTML = `
+        <div class="flex flex-col justify-center items-center bg-error_div rounded-3xl gap-6 py-24">
+        <img class="w-36" src="./images/error.webp" alt="">
+        <h3 class="font-bold text-3xl text-text_01">No Information Available</h3>
+        <p class="font-normal w-9/12 text-center text-text_03">
+            No information available at this time. Please check back later for updates or more details on the subject. We apologize for any inconvenience and appreciate your understanding as we work to provide further details.
+        </p>
+     </div>
+        `;
+        return;
+
+    } else{
+        dataContainer.classList.add("grid");
+    }
+
     pets.forEach(pet => {
         const petCard = document.createElement('div');
         petCard.className = "card box-border border bg-white rounded-xl shadow-lg overflow-hidden p-4";
@@ -59,3 +76,53 @@ function displayError() {
     const dataContainer = document.getElementById('data-container');
     dataContainer.innerHTML = '<p class="text-red-500">Error loading data.</p>';
 }
+
+
+
+const loadAllCetegory = async() => {
+    const apiUrl2 = 'https://openapi.programming-hero.com/api/peddy/categories';
+    
+    // Fetch and display data when the DOM is loaded
+    await fetchData2(apiUrl2);
+}
+async function fetchData2(apiUrl2) {
+    try {
+        const response = await fetch(apiUrl2);
+        const data = await response.json();
+        displayCategories(data.categories);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        displayError(); // Call function to display error message
+    }
+}
+
+const loadCetegoryPets = (category) => {
+    //alert(category);
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    .then((response) => response.json())
+    .then((data) => displayData(data.data))
+    .catch((error) => console.log(error));
+}
+
+const displayCategories = (data) => {
+    const catagoryContainer = document.getElementById("catagory");
+
+    data.forEach((item) => {
+        // create a Button
+        const buttonContainer = document.createElement("div")
+        // buttonContainer.classList = "btn";
+        buttonContainer.innerHTML =`
+        <button onclick="loadCetegoryPets('${item.category}')" class="btn text-center justify-center">
+         <img class="h-1/2" src="${item.category_icon}" alt="${item.category} Icon">
+            <div class="text-center mt-2">
+                <h3 class="font-bold text-lg">${item.category}</h3>
+            </div>
+        </button>    
+        `;
+        // add button to catagory caontainer
+        catagoryContainer.append(buttonContainer);
+    });
+}
+
+
+loadAllCetegory()
